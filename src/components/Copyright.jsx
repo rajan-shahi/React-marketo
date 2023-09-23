07a4c40c1ca705277af0f050e8304396e332f2b5
rajan <rajanshahi1267@gmail.com>
2023-09-22 23:27:33 -0700

    add

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -3,7 +3,7 @@ import { HiArrowUp } from "react-icons/hi";
 import { Link } from "react-scroll";
 const Copyright = () => {
   return (
-    <div className=" flex justify-between py-6 px-4 md:px-20 bg-gray-100">
+    <div className=" flex justify-between py-6 px-4 md:px-20 bg-gray-200">
       <div className=" text-sm text-gray-500">
         <h1>Copyrights By rajan bahadur shahi - 2023</h1>
       </div>
