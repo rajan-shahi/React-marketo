e6d36aee2cad4f54a5d8b5ac49c0b6e175fbc7ef
rajan <rajanshahi1267@gmail.com>
2023-09-24 18:21:56 -0700

    add data base

--- src/components/DicountProducr.jsx
+++ src/components/DicountProducr.jsx
@@ -1,12 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import admin from "../assets/ad-min.jpg";
-import sho1 from "../assets/sho1.jpg";
-import sho2 from "../assets/sho2.jpg";
-import sho3 from "../assets/sho3.jpg";
-import sho4 from "../assets/sho4.jpg";
-import sho5 from "../assets/sho5.jpg";
-import sho6 from "../assets/sho6.jpg";
+import { discounts40, discounts57, discounts75, discounts83 } from './../data/Data';
 const DicountProducr = () => {
+  const [list,setList]=useState(discounts40)
   return (
     <div className=" flex justify-center pt-10 md:pb-20 md:px-0 px-4">
       <div className=" md:flex justify-center  gap-10 w-full md:w-10/12">
@@ -21,34 +17,34 @@ const DicountProducr = () => {
               <h1>Discount Product</h1>
             </div>
             <div className=" flex md:gap-8 justify-between text-xl text-gray-500">
-              <h2 className=" cursor-pointer  hover:text-orange-400">
+              <h2  onClick={()=> setList(discounts40)} className=" cursor-pointer  hover:text-orange-500">
                 40% Off
               </h2>
-              <h2 className=" cursor-pointer  hover:text-orange-400">
+              <h2   onClick={()=> setList(discounts57)}  className=" cursor-pointer  hover:text-orange-500">
                 57% Off
               </h2>
-              <h3 className=" cursor-pointer  hover:text-orange-400">
+              <h3  onClick={()=> setList(discounts75)} className=" cursor-pointer  hover:text-orange-500">
                 75% Off
               </h3>
-              <h2 className=" cursor-pointer  hover:text-orange-400">
+              <h2  onClick={()=> setList(discounts83)} className=" cursor-pointer  hover:text-orange-500">
                 83% Off
               </h2>
             </div>
           </div>
           {/* card */}
           <div className=" grid md:grid-cols-3 md:justify-between  gap-5  py-6">
-            {cardshows.map((cardshow, index) => (
+            {list.map((discounts40, index) => (
               <div
                 key={index}
                 className=" flex flex-col justify-center   hover:shadow-lg hover:shadow-slate-200 px-4 pb-8"
               >
                 <div>
-                  <img className=" cursor-pointer" src={cardshow.image} alt="" />
+                  <img className=" cursor-pointer" src={discounts40.image} alt="" />
                 </div>
                 <div className=" flex flex-col gap-1  items-center">
-                  <h2 className=" text-gray-800   font-bold hover:text-blue-600 cursor-pointer">{cardshow.model}</h2>
-                  <h2>{cardshow.p}</h2>
-                  <p className=" text-orange-400 text-lg">{cardshow.price}</p>
+                  <h2 className=" text-gray-800   font-bold hover:text-blue-600 cursor-pointer">{discounts40.model}</h2>
+                  <h2>{discounts40.p}</h2>
+                  <p className=" text-orange-400 text-lg">{discounts40.price}</p>
                 </div>
               </div>
             ))}
@@ -61,47 +57,3 @@ const DicountProducr = () => {
 
 export default DicountProducr;
 
-const cardshows = [
-  {
-    image: sho5,
-
-    model: "Notebook screen Z51-",
-    p: "2QF-621XLP",
-    price: "$400-$130.00",
-  },
-  {
-    image: sho6,
-    name: "Notebook",
-    model: "Notebook screen Z51-",
-    p: "70 40k60",
-    price: "&49449.00",
-  },
-  {
-    image: sho1,
-    name: "Head Phones",
-    model: "Notebook screen Z51-",
-    p: "JHTBUK",
-    price: "&42012.00",
-  },
-  {
-    image: sho2,
-
-    model: "Notebook screen Z51-",
-    p: "70 40k60",
-    price: "&49449.00",
-  },
-  {
-    image: sho3,
-
-    model: "Notebook screen Z51-",
-    p: "725lku-210k6",
-    price: "&102362.00",
-  },
-  {
-    image: sho4,
-
-    model: "Notebook screen Z51-",
-    p: "40klKPD45",
-    price: "&10,000.00",
-  },
-];
--- src/data/Data.js
+++ src/data/Data.js
@@ -2,34 +2,193 @@ import sho1 from "../assets/sho1.jpg";
 import sho2 from "../assets/sho2.jpg";
 import sho3 from "../assets/sho3.jpg";
 import sho4 from "../assets/sho4.jpg";
+import sho5 from "../assets/sho5.jpg";
+import sho6 from "../assets/sho6.jpg";
 
-export const cards = [
+
+
+ export const discounts40 = [
+  {
+    image: sho5,
+
+    model: "Notebook screen Z51-",
+    p: "2QF-621XLP",
+    price: "$400-$130.00",
+  },
+  {
+    image: sho6,
+    name: "Notebook",
+    model: "Notebook screen Z51-",
+    p: "70 40k60",
+    price: "&49449.00",
+  },
   {
     image: sho1,
     name: "Head Phones",
-    model: "Notebook Blgscreen Z51-70",
-    p: "70",
+    model: "Notebook screen Z51-",
+    p: "JHTBUK",
     price: "&42012.00",
   },
   {
     image: sho2,
-    name: "Mobile Projector",
-    model: "Notebook Blgscreen Z51-70",
+
+    model: "Notebook screen Z51-",
     p: "70 40k60",
     price: "&49449.00",
   },
   {
     image: sho3,
-    name: "Further Products",
-    model: "Notebook Blgscreen Z51-70",
-    p: "70k6",
+
+    model: "Notebook screen Z51-",
+    p: "725lku-210k6",
     price: "&102362.00",
   },
   {
     image: sho4,
-    name: "Featured Product Laptop",
-    model: "Notebook Blgscreen Z51-70",
-    p: "40k45",
+
+    model: "Notebook screen Z51-",
+    p: "40klKPD45",
+    price: "&10,000.00",
+  },
+];
+
+
+
+export const discounts57 = [
+  {
+    image: sho4,
+
+    model: "Notebook screen Z51-",
+    p: "2QF-621XLP",
+    price: "$400-$130.00",
+  },
+  {
+    image: sho3,
+    name: "Notebook",
+    model: "Notebook screen Z51-",
+    p: "70 40k60",
+    price: "&49449.00",
+  },
+  {
+    image: sho2,
+    name: "Head Phones",
+    model: "Notebook screen Z51-",
+    p: "JHTBUK",
+    price: "&42012.00",
+  },
+  {
+    image: sho1,
+
+    model: "Notebook screen Z51-",
+    p: "70 40k60",
+    price: "&49449.00",
+  },
+  {
+    image: sho6,
+
+    model: "Notebook screen Z51-",
+    p: "725lku-210k6",
+    price: "&102362.00",
+  },
+  {
+    image: sho5,
+
+    model: "Notebook screen Z51-",
+    p: "40klKPD45",
+    price: "&10,000.00",
+  },
+];
+
+
+
+export const discounts75 = [
+  {
+    image: sho5,
+
+    model: "Notebook screen Z51-",
+    p: "2QF-621XLP",
+    price: "$400-$130.00",
+  },
+  {
+    image: sho4,
+    name: "Notebook",
+    model: "Notebook screen Z51-",
+    p: "70 40k60",
+    price: "&49449.00",
+  },
+  {
+    image: sho3,
+    name: "Head Phones",
+    model: "Notebook screen Z51-",
+    p: "JHTBUK",
+    price: "&42012.00",
+  },
+  {
+    image: sho2,
+
+    model: "Notebook screen Z51-",
+    p: "70 40k60",
+    price: "&49449.00",
+  },
+  {
+    image: sho1,
+
+    model: "Notebook screen Z51-",
+    p: "725lku-210k6",
+    price: "&102362.00",
+  },
+  {
+    image: sho6,
+
+    model: "Notebook screen Z51-",
+    p: "40klKPD45",
+    price: "&10,000.00",
+  },
+];
+
+
+
+export const discounts83 = [
+  {
+    image: sho6,
+
+    model: "Notebook screen Z51-",
+    p: "2QF-621XLP",
+    price: "$400-$130.00",
+  },
+  {
+    image: sho5,
+    name: "Notebook",
+    model: "Notebook screen Z51-",
+    p: "70 40k60",
+    price: "&49449.00",
+  },
+  {
+    image: sho4,
+    name: "Head Phones",
+    model: "Notebook screen Z51-",
+    p: "JHTBUK",
+    price: "&42012.00",
+  },
+  {
+    image: sho2,
+
+    model: "Notebook screen Z51-",
+    p: "70 40k60",
+    price: "&49449.00",
+  },
+  {
+    image: sho3,
+
+    model: "Notebook screen Z51-",
+    p: "725lku-210k6",
+    price: "&102362.00",
+  },
+  {
+    image: sho1,
+
+    model: "Notebook screen Z51-",
+    p: "40klKPD45",
     price: "&10,000.00",
   },
 ];
