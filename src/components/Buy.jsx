f3b85078c957fa65e7006b673be5142bd54e1e1a
rajan <rajanshahi1267@gmail.com>
2023-09-24 19:58:32 -0700

    add

--- src/components/Buy.jsx
+++ src/components/Buy.jsx
@@ -13,13 +13,13 @@ const Buy = () => {
               <h1 className=" md:text-4xl text-2xl   text-gray-200  font-bold">
                 Join Now & earns 50% reward on every orders{" "}
               </h1>
-              <p className=" text-2xl text-white py-4">$340.000</p>
+              <p className=" text-xl   text-gray-200  font-bold py-4">$340.000</p>
             </div>
-            <div className="    grid md:grid-cols-2 grid-cols-1 md:gap-1 gap-6">
-              <button className="   bg-red-700  w-56 py-3  text-white  hover:bg-orange-400  font-bold">
+            <div className="  md:w-max    grid md:grid-cols-2 grid-cols-1 md:gap-1 gap-6">
+              <button className=" flex justify-center   bg-red-700  w-full md:w-52 py-3  text-white  hover:bg-orange-400  font-bold">
                 Buy Now
               </button>
-              <button className="  bg-black  w-60 py-3  text-white  font-bold border-2 border-white hover:border-orange-400">
+              <button className="  flex justify-center bg-black w-full  md:w-60 py-3  text-white  font-bold border-2 border-white hover:border-orange-400 hover:bg-transparent">
                 VIEW COLLECTIONS
               </button>
             </div>
