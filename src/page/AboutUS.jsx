cc1bd1871f8fe7a0199c439f2ce353f6d12a7de9
rajan <rajanshahi1267@gmail.com>
2023-09-26 08:27:02 -0700

    add

--- src/page/AboutUS.jsx
+++ src/page/AboutUS.jsx
@@ -29,12 +29,12 @@ const AboutUS = () => {
       </div>
       {/* history section */}
       <div className=" flex md:px-0 px-4 justify-center  py-14  md:py-28">
-        <div className=" md:flex  w-full md:w-9/12  items-center ">
+        <div className=" md:flex   w-full md:w-9/12  items-center  ">
           {/* leftbar */}
           <div className=" flex md:flex-1 flex-col gap-6">
-            <h1 className=" text-lg  text-blue-700">OUR HISTORY</h1>
+            <h1 className=" md:text-lg  text-sm text-blue-700">OUR HISTORY</h1>
 
-            <h2 className="  text-4xl text-gray-700 font-bold">
+            <h2 className="  md:text-4xl  text-2xl text-gray-700 font-bold">
               Creative and renovate fashion trends
             </h2>
 
@@ -45,23 +45,23 @@ const AboutUS = () => {
               directed convergence without revolutionary ROI.
             </p>
 
-            <div className=" flex gap-10 pt-12">
+            <div className="  grid md:grid-cols-3 grid-cols-2 md:gap-10  gap-6 md:pt-12 pt-5">
               <div className=" flex  flex-col gap-1 ">
-                <h1 className=" text-5xl text-blue-600 font-bold">12</h1>
+                <h1 className=" md:text-5xl text-3xl  text-blue-600 font-bold">12</h1>
                 <p className="  text-sm text-gray-500">Years Experience</p>
               </div>
               <div className=" flex  flex-col gap-1 ">
-                <h1 className=" text-5xl text-blue-600 font-bold">20K</h1>
+                <h1 className=" md:text-5xl  text-3xl text-blue-600 font-bold">20K</h1>
                 <p className="  text-sm text-gray-500">Happy Customers</p>
               </div>
               <div className=" flex  flex-col gap-1 ">
-                <h1 className=" text-5xl text-blue-600 font-bold">100%</h1>
+                <h1 className=" md:text-5xl  text-3xl text-blue-600 font-bold">100%</h1>
                 <p className="  text-sm text-gray-500">Clients Satisfaction</p>
               </div>
             </div>
           </div>
           {/* rightbar */}
-          <div className="  flex md:flex-1 justify-start  md:justify-end">
+          <div className="  md:pt-0 pt-8  md:pb-0 pb-8 flex md:flex-1 justify-start  md:justify-end">
             <img src={about} alt="" />
           </div>
         </div>
@@ -69,23 +69,23 @@ const AboutUS = () => {
 
       {/* VISION SECTION */}
       <div className=" py-10  md:px-0 px-4 bg-gray-100 flex justify-center">
-        <div className=" md:flex  py-12 w-full md:w-9/12">
+        <div className=" md:flex  md:py-12 w-full md:w-9/12">
           {/* left */}
-          <div className="  md:flex-1">
+          <div className="  md:flex-1 md:pb-0 pb-5">
             <img src={about21} alt="" />
           </div>
           {/* right */}
-          <div className=" flex flex-col md:flex-1  gap-6">
-            <h1 className=" text-lg text-blue-700">Our vision</h1>
-            <h2 className=" text-4xl text-gray-700  font-bold">
+          <div className=" flex flex-col md:flex-1  md:gap-6   gap-4">
+            <h1 className=" md:text-lg text-blue-700">Our vision</h1>
+            <h2 className=" md:text-4xl text-2xl text-gray-700  font-bold">
               We are marketpress
             </h2>
-            <p className=" text-md text-gray-500">
+            <p className=" md:text-md  text-sm text-gray-500">
               Dynamically procrastinate B2C users after installed base benefits.
               Dramatically visualize customer directed convergence without
               revolutionary ROI.
             </p>
-            <div className=" flex flex-col gap-3 py-10 text-md  text-black   ">
+            <div className=" flex flex-col gap-3 md:py-10 py-6 text-md  text-black   ">
               <h1>Credibly innovate granular internal</h1>
               <h1>Grantedly underconstructions reloaded</h1>
               <h1>Interactively procrastinate high-payoff</h1>
@@ -98,16 +98,16 @@ const AboutUS = () => {
       <div className=" md:px-0 px-4 flex py-16 justify-center">
         <div className=" flex  flex-col justify-center w-full md:w-9/12">
           <div className=" flex flex-col gap-1">
-            <h1 className=" text-md text-blue-700">ANGELS</h1>
-            <h2 className=" text-3xl text-gray-700 font-bold">
+            <h1 className=" md:text-md text-sm text-blue-700">ANGELS</h1>
+            <h2 className=" md:text-3xl  text-2xl text-gray-700 font-bold">
               Meet with our Team
             </h2>
           </div>
           <div className=" grid grid-cols-1 md:grid-cols-4  gap-4 py-4">
             {teams.map((team, index) => (
-              <div key={index} className=" flex group  flex-col gap-5  py-8">
+              <div key={index} className=" flex group  flex-col gap-5  py-6 md:py-8">
                 <div>
-                  <img src={team.image} alt="" />
+                  <img className=" flex w-full" src={team.image} alt="" />
                 </div>
                 <div className=" opacity-0 group-hover:opacity-100  duration-1000 absolute flex gap-4  pl-20 py-36 text-black font-bold text-2xl  ">
                   <span className=" cursor-pointer">
