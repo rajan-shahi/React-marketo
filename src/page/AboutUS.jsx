7692666505ad92f9c8cec9a7918fc166fb7ff7cd
rajan <rajanshahi1267@gmail.com>
2023-09-26 02:25:18 -0700

    add

--- src/page/AboutUS.jsx
+++ src/page/AboutUS.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import about from "../assets/about_us_img_11.png";
 import about21 from "../assets/about_us_img_21.png";
+import team11 from "../assets/team_11.png";
+import team21 from "../assets/team_21.png";
+import team31 from "../assets/team_31.png";
+import team41 from "../assets/team_41.png";
 import { AiOutlineDoubleRight } from "react-icons/ai";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
 const AboutUS = () => {
   return (
     // navbar section
     <div className="  py-20">
-      <div className=" flex    bg bg-[#FBE4D8] justify-center">
+      <div className=" flex   bg-gray-100 justify-center">
         <div className=" w-9/12 flex py-10 items-center text-sm text-gray-600 font-bold">
           <div className=" flex gap-4">
             <h1>Home</h1>
@@ -58,28 +65,57 @@ const AboutUS = () => {
       </div>
 
       {/* VISION SECTION */}
-      <div className=" py-10 bg-gray-200 flex justify-center">
+      <div className=" py-10 bg-gray-100 flex justify-center">
         <div className=" flex  py-12 w-9/12">
-        {/* left */}
-        <div className="  flex-1">
-          <img src={about21} alt="" />
-        </div>
-        {/* right */}
-        <div className=" flex flex-col flex-1  gap-6">
-          <h1 className=" text-lg text-blue-600">Our vision</h1>
-          <h2 className=" text-4xl text-gray-700  font-bold">We are marketpress</h2>
-          <p  className=" text-md text-gray-500">
-            Dynamically procrastinate B2C users after installed base benefits.
-            Dramatically visualize customer directed convergence without
-            revolutionary ROI.
-          </p>
-          <div className=" flex flex-col gap-3 py-10 text-md text-gray-800 font-bold">
-            <h1>Credibly innovate granular internal</h1>
-            <h1>Grantedly underconstructions reloaded</h1>
-            <h1>Interactively procrastinate high-payoff</h1>
-            <h1>Completely synergize resource taxing relationships</h1>
+          {/* left */}
+          <div className="  flex-1">
+            <img src={about21} alt="" />
+          </div>
+          {/* right */}
+          <div className=" flex flex-col flex-1  gap-6">
+            <h1 className=" text-lg text-blue-600">Our vision</h1>
+            <h2 className=" text-4xl text-gray-700  font-bold">
+              We are marketpress
+            </h2>
+            <p className=" text-md text-gray-500">
+              Dynamically procrastinate B2C users after installed base benefits.
+              Dramatically visualize customer directed convergence without
+              revolutionary ROI.
+            </p>
+            <div className=" flex flex-col gap-3 py-10 text-md  text-black   ">
+              <h1>Credibly innovate granular internal</h1>
+              <h1>Grantedly underconstructions reloaded</h1>
+              <h1>Interactively procrastinate high-payoff</h1>
+              <h1>Completely synergize resource taxing relationships</h1>
+            </div>
           </div>
         </div>
+      </div>
+      {/* MEET SECTIONS */}
+      <div className=" flex py-16 justify-center">
+        <div className=" flex  flex-col justify-center w-9/12">
+          <div>
+            <h1 className=" text-md text-blue-600">ANGELS</h1>
+            <h2 className=" text-4xl text-gray-700 font-bold">Meet with our Team</h2>
+          </div>
+          <div className=" grid grid-cols-4 gap-4 py-4">
+            {teams.map((team,index)=>(
+              <div key={index} className=" flex group  flex-col gap-5  py-8">
+                <div>
+                  <img src={team.image} alt="" />
+                </div>
+                <div className=" opacity-0 group-hover:opacity-100  duration-1000 absolute flex gap-4  pl-20 py-36 text-black font-bold text-2xl  ">
+                  <span className=" cursor-pointer"><FaFacebookF/></span>
+                  <span className=" cursor-pointer"><FaTwitterSquare/></span>
+                  <span className=" cursor-pointer"><FaInstagramSquare/></span>
+                </div>
+                <div className=" flex flex-col gap-1 items-center">
+                  <h1 className=" text-sm text-blue-700 font-bold">{team.post}</h1>
+                  <h2 className=" text-xl text-gray-800 font-bold">{team.name}</h2>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
@@ -87,3 +123,63 @@ const AboutUS = () => {
 };
 
 export default AboutUS;
+
+
+
+
+
+const teams = [
+  {
+    image: team11,
+    post: "FOUNDER",
+    name: "Tony Williamson",
+   
+    
+  },
+  {
+    image: team21,
+    post: "CEO",
+    name: "William Hanah",
+ 
+  },
+  
+  {
+    image: team31,
+    post: "ADVISOR",
+    name: "JInglo Jelian",
+   
+  },
+  {
+    image: team41,
+    post: "DESIGNER",
+    name: "Jony Smith",
+  
+  },
+
+  {
+    image: team31,
+    post: "FOUNDER",
+    name: "Tony Williamson",
+   
+    
+  },
+  {
+    image: team41,
+    post: "CEO",
+    name: "William Hanah",
+ 
+  },
+  
+  {
+    image: team11,
+    post: "ADVISOR",
+    name: "JInglo Jelian",
+   
+  },
+  {
+    image: team21,
+    post: "DESIGNER",
+    name: "Jony Smith",
+  
+  },
+];
