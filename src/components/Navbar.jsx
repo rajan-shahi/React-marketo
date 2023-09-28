59f4aee96f17997c13b54b9327342017813fa429
rajan <rajanshahi1267@gmail.com>
2023-09-28 03:20:14 -0700

    add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,6 @@ const Navbar = () => {
         </div>
         {/* right */}
         <div className=" md:flex  hidden gap-10">
-          rrajan
           <Link to={"/about"}>
             <div className=" flex gap-1 items-center">
               <h1 className=" text-sm font-bold text-gray-600 cursor-pointer hover:text-orange-400">
