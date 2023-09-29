102f101d353e23e5c3fd7e5c1821e45e7eee2f85
rajan <rajanshahi1267@gmail.com>
2023-09-29 03:56:42 -0700

    add

--- src/components/PopularsProduct.jsx
+++ src/components/PopularsProduct.jsx
@@ -18,7 +18,7 @@ const PopularsProduct = () => {
               <RiExpandLeftRightLine size={50} />
             </span>
           </div>
-          rajan
+    
           <div className=" flex flex-col gap-8 pt-4">
             {populars.map((popular,index)=>(
                 <div key={index} className="  flex  gap-3 items-center">
