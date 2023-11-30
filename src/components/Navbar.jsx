a5a0e105cc2807a1da864ff02859328514649e8a
rajan <rajanshahi1267@gmail.com>
2023-11-30 06:53:06 -0700

    add another components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,6 +23,7 @@ const Navbar = () => {
                  Rajan Bahadur Shahi
                  lokendra chaulagain
                  git bista
+                 tapendra bahadur shahi
               </h1>
               <span className=" flex  items-center pt-2  text-gray-500 cursor-pointer hover:text-orange-400 ">
                 <BsChevronDown />
