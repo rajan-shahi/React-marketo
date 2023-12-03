1fb9e77aefaebd5c740fb38743c147a2df3b480f
rajan <rajanshahi1267@gmail.com>
2023-12-03 11:59:46 -0700

    add

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { SlSocialFacebook } from "react-icons/sl";
 import { FaLinkedinIn } from "react-icons/fa";
@@ -6,101 +6,99 @@ import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <div className=" flex justify-center   ">
-    <div className=" flex   flex-col gap-2 w-full md:w-3/12 shadow-lg   shadow-gray-300  pb-20 rounded-lg ">
-      <div className=" flex flex-col justify-center items-center">
-        <h1 className=" text-2xl text-gray-600">Login</h1>
-        <h2 className=" text-sm text-gray-500">
-          Don't have an account?{" "}
-          <span className=" text-blue-500 cursor-pointer text-lg hover:text-orange-400">
-            <Link to={"/signup"}> sign up</Link>
-          </span>
-        </h2>
-      </div>
-      <div className=" flex  md:px-4 flex-col md:gap-5  gap-6 justify-center items-center">
-        <div className=" flex flex-col">
-          <span className=" text-md text-gray-600">Email</span>
-          <input
-            name="email"
-            className=" w-[350px] border  border-gray-400 outline-orange-400 px-1 md:py-1 py-2 rounded-lg  placeholder:text-sm"
-            type="email"
-            placeholder="Enter Your email"
-          />
+      <div className=" flex   flex-col gap-2 w-full md:w-3/12 shadow-lg   shadow-gray-300  pb-20 rounded-lg ">
+        <div className=" flex flex-col justify-center items-center">
+          <h1 className=" text-2xl text-gray-600">Login</h1>
+          <h2 className=" text-sm text-gray-500">
+            Don't have an account?{" "}
+            <span className=" text-blue-500 cursor-pointer text-lg hover:text-orange-400">
+              <Link to={"/signup"}> sign up</Link>
+            </span>
+          </h2>
         </div>
-        <div className=" flex flex-col">
-          <span className=" text-md text-gray-600 ">Password</span>
-          <input
-            name="password"
-            className=" w-[350px] border  border-gray-400 outline-orange-400 px-1 md:py-1  py-2 rounded-lg  placeholder:text-sm"
-            type="password"
-            placeholder="Enter Your password"
-          />
-        </div>
-        <div>
-          <button className=" w-[350px] border  border-gray-400 outline-orange-400 px-1 py-2 md:py-1 rounded-lg  bg-blue-400 text-white">
-            Login
-          </button>
-        </div>
-        <div>
-          <h1>or</h1>
-        </div>
-        <div className="w-[350px] border  border-gray-400  py-3 rounded-lg  flex justify-center items-center">
-          <span className=" px-1  cursor-pointer">
-            <a href="https://www.google.com" target="-blank">
-              {" "}
-              <FcGoogle size={20} />
-            </a>
-          </span>
-          <h1 className=" text-sm  text-gray-500  cursor-pointer">
-            <a href="https://www.google.com" target="-blank">
-              {" "}
-              Sign in with Google{" "}
-            </a>
-          </h1>
-        </div>
-        <div className="w-[350px] border  border-gray-400   py-3 rounded-lg flex justify-center items-center">
-          <span className="  text-blue-500 px-1 cursor-pointer">
-            <a
-              href="https://www.facebook.com
+        <div className=" flex  md:px-4 flex-col md:gap-5  gap-6 justify-center items-center">
+          <div className=" flex flex-col">
+            <span className=" text-md text-gray-600">Email</span>
+            <input
+              name="email"
+              className=" w-[350px] border  border-gray-400 outline-orange-400 px-3 md:py-1 py-2 rounded-lg  placeholder:text-sm"
+              type="email"
+              placeholder="Enter Your email"
+            />
+          </div>
+          <div className=" flex flex-col">
+            <span className=" text-md text-gray-600 ">Password</span>
+            <input
+              name="password"
+              className=" w-[350px] border  border-gray-400 outline-orange-400 px-3 md:py-1  py-2 rounded-lg  placeholder:text-sm"
+              type="password"
+              placeholder="Enter Your password"
+            />
+          </div>
+          <div>
+            <button className=" w-[350px] border  border-gray-400 outline-orange-400 px-1 py-2 md:py-1 rounded-lg  bg-blue-400 text-white">
+              Login
+            </button>
+          </div>
+          <div>
+            <h1>or</h1>
+          </div>
+          <div className="w-[350px] border  border-gray-400  py-3 rounded-lg  flex justify-center items-center">
+            <span className=" px-1  cursor-pointer">
+              <a href="https://www.google.com" target="-blank">
+                {" "}
+                <FcGoogle size={20} />
+              </a>
+            </span>
+            <h1 className=" text-sm  text-gray-500  cursor-pointer">
+              <a href="https://www.google.com" target="-blank">
+                {" "}
+                Sign in with Google{" "}
+              </a>
+            </h1>
+          </div>
+          <div className="w-[350px] border  border-gray-400   py-3 rounded-lg flex justify-center items-center">
+            <span className="  text-blue-500 px-1 cursor-pointer">
+              <a
+                href="https://www.facebook.com
 "
-              target="-blank"
-            >
-              <SlSocialFacebook size={20} />
-            </a>
-          </span>
-          <h1 className=" text-sm  text-gray-500  cursor-pointer">
-            <a href="https://www.facebook.com" target="-blank">
-              {" "}
-              Sign in with Facebook{" "}
-            </a>
-          </h1>
-        </div>
-        <div className="w-[350px] border  border-gray-400  py-3 rounded-lg  flex justify-center  items-center">
-          <span className=" text-blue-500 px-1 cursor-pointer">
-            <a
-              href="https://www.linkedin.com
+                target="-blank"
+              >
+                <SlSocialFacebook size={20} />
+              </a>
+            </span>
+            <h1 className=" text-sm  text-gray-500  cursor-pointer">
+              <a href="https://www.facebook.com" target="-blank">
+                {" "}
+                Sign in with Facebook{" "}
+              </a>
+            </h1>
+          </div>
+          <div className="w-[350px] border  border-gray-400  py-3 rounded-lg  flex justify-center  items-center">
+            <span className=" text-blue-500 px-1 cursor-pointer">
+              <a
+                href="https://www.linkedin.com
 "
-              target="-blank"
-            >
-              {" "}
-              <FaLinkedinIn size={20} />
-            </a>
-          </span>
-          <h1 className=" text-sm  text-gray-500   cursor-pointer">
-            <a
-              href="https://www.linkedin.com
-"
-              target="-blank"
-            >
-              {" "}
-              Sign in with Linkedin{" "}
-            </a>
-          </h1>
+                target="-blank"
+              >
+                {" "}
+                <FaLinkedinIn size={20} />
+              </a>
+            </span>
+            <h1 className=" text-sm  text-gray-500   cursor-pointer">
+              <a
+                href="https://www.linkedin.com"
+                target="-blank"
+              >
+                {" "}
+                Sign in with Linkedin{" "}
+              </a>
+            </h1>
+          </div>
         </div>
       </div>
     </div>
-    
-  </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
