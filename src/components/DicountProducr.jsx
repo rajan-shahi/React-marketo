fdd25298281a503f9ec8377f8336916623d797b6
rajan <rajanshahi1267@gmail.com>
2023-12-08 09:40:49 -0700

    Add

--- src/components/DicountProducr.jsx
+++ src/components/DicountProducr.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import admin from "../assets/ad-min.jpg";
 import {
+  discounts20,
+  discounts15,
+  discounts30,
   discounts40,
-  discounts57,
-  discounts75,
-  discounts83,
 } from "./../data/Data";
 
 const DicountProducr = () => {
-  const [list, setList] = useState(discounts40);
+  const [list, setList] = useState(discounts30);
   console.log(list);
-  
+
   return (
     <div className=" flex justify-center pt-10 md:pb-20 md:px-0 px-4">
       <div className=" md:flex justify-center  gap-10 w-full md:w-10/12">
@@ -26,6 +26,30 @@ const DicountProducr = () => {
             </div>
             <div className="  md:pt-0 pt-2 flex md:gap-8 justify-between text-lg text-gray-500">
               <h2
+                onClick={() => setList(discounts30)}
+                className={`${
+                  list.name === "dis30" ? "text-red-700" : ""
+                } cursor-pointer  font-bold `}
+              >
+                30% Off
+              </h2>
+              <h2
+                onClick={() => setList(discounts20)}
+                className={`${
+                  list.name === "dis20" ? "text-red-700" : ""
+                } cursor-pointer  font-bold `}
+              >
+                20% Off
+              </h2>
+              <h3
+                onClick={() => setList(discounts15)}
+                className={`${
+                  list.name === "dis15" ? "text-red-700" : ""
+                } cursor-pointer  font-bold `}
+              >
+                15% Off
+              </h3>
+              <h2
                 onClick={() => setList(discounts40)}
                 className={`${
                   list.name === "dis40" ? "text-red-700" : ""
@@ -33,26 +57,11 @@ const DicountProducr = () => {
               >
                 40% Off
               </h2>
-              <h2 onClick={() => setList(discounts57)} className="  ">
-                57% Off
-              </h2>
-              <h3
-                onClick={() => setList(discounts75)}
-                className=" cursor-pointer  font-bold  hover:text-red-700 "
-              >
-                75% Off
-              </h3>
-              <h2
-                onClick={() => setList(discounts83)}
-                className=" cursor-pointer  font-bold  hover:text-red-700 "
-              >
-                83% Off
-              </h2>
             </div>
           </div>
           {/* card */}
           <div className=" grid md:grid-cols-3 md:justify-between  gap-5  py-6">
-            {list?.items?.map((discounts40, index) => (
+            {list?.items?.map((discounts30, index) => (
               <div
                 key={index}
                 className=" flex flex-col justify-center   hover:shadow-lg hover:shadow-slate-200 px-4 pb-8"
@@ -60,17 +69,17 @@ const DicountProducr = () => {
                 <div>
                   <img
                     className=" cursor-pointer"
-                    src={discounts40.image}
+                    src={discounts30.image}
                     alt=""
                   />
                 </div>
                 <div className=" flex flex-col gap-1  items-center">
                   <h2 className=" text-gray-800   font-bold hover:text-blue-600 cursor-pointer">
-                    {discounts40.model}
+                    {discounts30.model}
                   </h2>
-                  <h2>{discounts40.p}</h2>
+                  <h2>{discounts30.p}</h2>
                   <p className=" text-orange-700  font-bold text-lg">
-                    {discounts40.price}
+                    {discounts30.price}
                   </p>
                 </div>
               </div>
--- src/data/Data.js
+++ src/data/Data.js
@@ -5,8 +5,8 @@ import sho4 from "../assets/sho4.jpg";
 import sho5 from "../assets/sho5.jpg";
 import sho6 from "../assets/sho6.jpg";
 
-export const discounts40 = {
-  name: "dis40",
+export const discounts30 = {
+  name: "dis30",
   items: [
     {
       image: sho5,
@@ -52,137 +52,144 @@ export const discounts40 = {
   ],
 };
 
-export const discounts57 = [
-  {
-    image: sho4,
+export const discounts15 = {
+  name: "dis15",
+  items: [
+    {
+      image: sho4,
 
-    model: "ASUS laptop inspire 2016 ",
-    p: "2QF-621XLP",
-    price: "$110-$240.00",
-  },
-  {
-    image: sho3,
-    name: "Notebook Blgscreen Z51-70 ",
-    model: "Notebook screen Z51-",
-    p: "40K6009",
-    price: "&49449.00-10.022",
-  },
-  {
-    image: sho2,
-    name: "NoteBook",
-    model: "ASUS laptop inspire 2016",
-    p: "JHTBUK40k",
-    price: "&42012.00-40k120",
-  },
-  {
-    image: sho1,
+      model: "ASUS laptop inspire 2016 ",
+      p: "2QF-621XLP",
+      price: "$110-$240.00",
+    },
+    {
+      image: sho3,
+      name: "Notebook Blgscreen Z51-70 ",
+      model: "Notebook screen Z51-",
+      p: "40K6009",
+      price: "&49449.00-10.022",
+    },
+    {
+      image: sho2,
+      name: "NoteBook",
+      model: "ASUS laptop inspire 2016",
+      p: "JHTBUK40k",
+      price: "&42012.00-40k120",
+    },
+    {
+      image: sho1,
 
-    model: "Notebook screen Z51-",
-    p: "70 4059695k60",
-    price: "&49449.00",
-  },
-  {
-    image: sho6,
+      model: "Notebook screen Z51-",
+      p: "70 4059695k60",
+      price: "&49449.00",
+    },
+    {
+      image: sho6,
 
-    model: "Notebook screen Z51-",
-    p: "725lku-21560k6",
-    price: "&102362.00-100k",
-  },
-  {
-    image: sho5,
+      model: "Notebook screen Z51-",
+      p: "725lku-21560k6",
+      price: "&102362.00-100k",
+    },
+    {
+      image: sho5,
 
-    model: "ASUS laptop inspire 2016-",
-    p: "40kl-LOK-KPD45",
-    price: "&10,000.00",
-  },
-];
+      model: "ASUS laptop inspire 2016-",
+      p: "40kl-LOK-KPD45",
+      price: "&10,000.00",
+    },
+  ],
+};
+export const discounts20 = {
+  name: "dis20",
+  items: [
+    {
+      image: sho5,
 
-export const discounts75 = [
-  {
-    image: sho5,
+      model: "Notebook screen Z51-",
+      p: "2QF-621XLP",
+      price: "$400-$130.00",
+    },
+    {
+      image: sho4,
+      name: "ASUS laptop inspire 2016",
+      model: "Notebook screen Z51-",
+      p: "70 40k60",
+      price: "&49449.00",
+    },
+    {
+      image: sho3,
+      name: "Head Phones",
+      model: "Notebook screen Z51-",
+      p: "JHTBUK",
+      price: "&42012.00",
+    },
+    {
+      image: sho2,
 
-    model: "Notebook screen Z51-",
-    p: "2QF-621XLP",
-    price: "$400-$130.00",
-  },
-  {
-    image: sho4,
-    name: "ASUS laptop inspire 2016",
-    model: "Notebook screen Z51-",
-    p: "70 40k60",
-    price: "&49449.00",
-  },
-  {
-    image: sho3,
-    name: "Head Phones",
-    model: "Notebook screen Z51-",
-    p: "JHTBUK",
-    price: "&42012.00",
-  },
-  {
-    image: sho2,
+      model: "ASUS laptop inspire 2016",
+      p: "70 40k60",
+      price: "&49449.00",
+    },
+    {
+      image: sho1,
 
-    model: "ASUS laptop inspire 2016",
-    p: "70 40k60",
-    price: "&49449.00",
-  },
-  {
-    image: sho1,
+      model: "Notebook screen Z51-",
+      p: "725lku-210k6",
+      price: "&102362.00-102",
+    },
+    {
+      image: sho6,
 
-    model: "Notebook screen Z51-",
-    p: "725lku-210k6",
-    price: "&102362.00-102",
-  },
-  {
-    image: sho6,
+      model: "ASUS laptop inspire 2016",
+      p: "40klKPD45 plk",
+      price: "&10,000.00-1023",
+    },
+  ],
+};
+export const discounts40 = {
+  name: "dis40",
+  items: [
+    {
+      image: sho6,
 
-    model: "ASUS laptop inspire 2016",
-    p: "40klKPD45 plk",
-    price: "&10,000.00-1023",
-  },
-];
+      model: "ASUS laptop inspire 2016",
+      p: "2QF-621XLP-1203",
+      price: "$400-$130.00",
+    },
+    {
+      image: sho5,
+      name: "Notebook",
+      model: "Notebook screen Z51-",
+      p: "70 PLZ 40k60",
+      price: "&49449.00",
+    },
+    {
+      image: sho4,
+      name: "ASUS laptop inspire 2016",
+      model: "Notebook screen Z51-",
+      p: "JHTBUK-6320",
+      price: "&42012.00",
+    },
+    {
+      image: sho2,
 
-export const discounts83 = [
-  {
-    image: sho6,
+      model: "Notebook screen Z51-",
+      p: "70 -120-40k60",
+      price: "&49449.00",
+    },
+    {
+      image: sho3,
 
-    model: "ASUS laptop inspire 2016",
-    p: "2QF-621XLP-1203",
-    price: "$400-$130.00",
-  },
-  {
-    image: sho5,
-    name: "Notebook",
-    model: "Notebook screen Z51-",
-    p: "70 PLZ 40k60",
-    price: "&49449.00",
-  },
-  {
-    image: sho4,
-    name: "ASUS laptop inspire 2016",
-    model: "Notebook screen Z51-",
-    p: "JHTBUK-6320",
-    price: "&42012.00",
-  },
-  {
-    image: sho2,
+      model: "ASUS laptop inspire 2016",
+      p: "725lku-210k6",
+      price: "&102362.00-362KL",
+    },
+    {
+      image: sho1,
 
-    model: "Notebook screen Z51-",
-    p: "70 -120-40k60",
-    price: "&49449.00",
-  },
-  {
-    image: sho3,
-
-    model: "ASUS laptop inspire 2016",
-    p: "725lku-210k6",
-    price: "&102362.00-362KL",
-  },
-  {
-    image: sho1,
-
-    model: "Notebook screen Z51-",
-    p: "40klKPD45",
-    price: "&10,000.00",
-  },
-];
+      model: "Notebook screen Z51-",
+      p: "40klKPD45",
+      price: "&10,000.00",
+    },
+  ],
+};
